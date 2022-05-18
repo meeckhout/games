2cef9c4b14919305f369c5c77436135f1b5596d8
meeckhout <marthe.eeckhout@gmail.com>
2022-05-18 12:18:21 +0200

    Add html and styling

---
+++ 1. Rock Paper Scissors/script.js
@@ -0,0 +1,10 @@
+//TODO: Three buttons that the player can press to use rock, paper or scissors
+
+
+//TODO: Play button so that computer picks one randomly
+
+
+//TODO: Announce a winner by using HTML message
+
+
+//TODO: Possible outcomes
