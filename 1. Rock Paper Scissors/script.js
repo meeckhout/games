9dea10023d2604262f01c83d4cc0dcccac88e92f
meeckhout <marthe.eeckhout@gmail.com>
2022-05-18 15:54:27 +0200

    Add functions

--- 1. Rock Paper Scissors/script.js
+++ 1. Rock Paper Scissors/script.js
@@ -4,11 +4,13 @@ const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => { 
     userChoice = event.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice() {
@@ -26,5 +28,27 @@ function generateComputerChoice() {
     computerChoiceDisplay.innerHTML = computerChoice
 }
 
-
-
+function getResult () {
+    if (computerChoice === userChoice) {
+        result = 'its a draw!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = 'you win!'
+    }
+    if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = 'you lost!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors') {
+        result = 'you win!'
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = 'you lost!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = 'you win!'
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = 'you lost!'
+    }
+    resultDisplay.innerHTML = result
+}
