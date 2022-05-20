f698c41ffa70fa49634e11df37e48a3cb142c791
meeckhout <marthe.eeckhout@gmail.com>
2022-05-20 09:53:14 +0200

    Add function

--- 2. 21 Card Game/script.js
+++ 2. 21 Card Game/script.js
@@ -5,10 +5,12 @@ const dealer = document.querySelector('.dealer')
 const pickCard = document.getElementById('pick')
 
 let card
+let cardTwo
 
 // TODO: Ask user if they want a card
 pickCard.addEventListener('click', (event) => {
     console.log(generateCard());
+    console.log(getRandomNumber());
 })
 
 function generateCard() {
@@ -16,3 +18,11 @@ function generateCard() {
 
     console.log(card);
 }
+
+//TODO: Add a function that draws a random number between 1 and 13
+function getRandomNumber() {
+    cardTwo = Math.floor(Math.random() *  (13 - 1 + 1) + 1)
+    console.log(cardTwo);
+}
+
+//TODO: Add a function that counts the card on the table
