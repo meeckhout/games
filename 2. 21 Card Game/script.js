f8899b07af12924c98ba7ed7e8e2b58cad2cc0b2
meeckhout <marthe.eeckhout@gmail.com>
2022-05-20 16:10:34 +0200

    WIP i don't know anymore

--- 2. 21 Card Game/script.js
+++ 2. 21 Card Game/script.js
@@ -1,6 +1,5 @@
 
-// const user = document.querySelector('.user')
-const userCard = document.querySelector('.user')
+const userCard = document.querySelector('.user-left')
 const dealer = document.querySelector('.dealer')
 const play = document.getElementById('play')
 const hit = document.getElementById('hit')
@@ -10,29 +9,36 @@ let hitMe = true
 let card
 let cardNumber
 let drawCard
-
+let total = cardNumber
 
 // TODO: Ask user if they want a card
 play.addEventListener('click', (event) => {
     console.log(getRandomNumber());
-    console.log(countCards());
+    console.log(dealCards());
+    // console.log(countCards());
 })
-
 
 //TODO: Add a function that draws a random number between 1 and 13
 function getRandomNumber() {
-    cardNumber = Math.floor(Math.random() * 11) + 1;
-    drawCard = cardNumber;
-
-    if (drawCard != 21) {
-        hitMe = confirm(`You have ${drawCard}. Add more?`)
-    }
+    cardNumber = Math.floor(Math.random() * 11 + 1)
+    return cardNumber
 }
 
-function countCards() {
-    for (let i = 0; i < 3; i++) {
-        getRandomNumber();
-        drawCard += cardNumber
-    } 
+function dealCards() {
+    total += Math.floor(Math.random() * 10 + 1)
+    document.getElementById('user-left').value.innerText = total
 }
 
+
+    // if (drawCard != 21) {
+    //     hitMe = confirm(`You have ${drawCard}. Add more?`)
+    //     // userCard.innerHTML = drawCard
+    // }
+
+
+// function countCards() {
+//     for (let i = 0; i < 3; i++) {
+//         getRandomNumber();
+//         total += card
+//     } 
+// }
