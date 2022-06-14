b94cbd168adc0653a1ab25419c055ad845477c21
meeckhout <marthe.eeckhout@gmail.com>
2022-06-14 14:14:53 +0200

    Redo js

---
+++ 2. 21 Card Game/app.js
@@ -0,0 +1,54 @@
+let deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+
+let playerHand
+let dealerHand
+
+const drawRandomCard = (deck) => {
+    const randomIndex = Math.floor(Math.random() * deck.length)
+    return deck[randomIndex]
+}
+
+document.getElementById('play').addEventListener('click', event => {
+    playerHand = [drawRandomCard(deck)]
+})
+
+const startGame = () => {
+    playerHand = [drawRandomCard(deck), drawRandomCard(deck)]
+    dealerHand = [drawRandomCard(deck), drawRandomCard(deck)]
+}
+
+const getHandValue = (hand) => {
+    let sum = 0
+    for (let i = 0; i <hand.length; i++){
+        sum = sum + hand[i]
+    }
+    return sum
+}
+
+const hit = () => {
+    playerHand.push(drawRandomCard(deck))
+    if (getHandValue(playerHand) > 21) {
+        document.getElementById('game-status').innerHTML = "BUST!"
+    }
+}
+
+const stay = () => {
+    dealerHand.push(drawRandomCard(deck))
+    if (getHandValue(dealerHand) > 21) {
+        console.log("BUST!")
+    }
+}
+
+
+document.getElementById('refresh').addEventListener('click', event => {
+    window.location.reload()
+    }
+)
+
+document.getElementById('player-hand').innerHTML = playerHand
+document.getElementById('player-hand-value').innerHTML = getHandValue(playerHand)
+document.getElementById('dealer-hand').innerHTML = dealerHand
+document.getElementById('dealer-hand-value').innerHTML = getHandValue(dealerHand)
--- 2. 21 Card Game/index.js
+++ 2. 21 Card Game/index.js
@@ -1,55 +1,24 @@
-const deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-                2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-                2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
-                2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+let deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11,
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 
-let playerHand
-let dealerHand
-
-const drawRandomCard = (deck) => {
+//Press start to begin game and get random card
+document.getElementById('start').addEventListener('click', event => {
     const randomIndex = Math.floor(Math.random() * deck.length)
-    return deck[randomIndex]
+    document.getElementById('player-hand').innerHTML = deck[randomIndex]
+    document.getElementById('dealer-hand').innerHTML = deck[randomIndex]
+})
+
+//Press 'hit' button and get new card
+const handValue = () => {
+    
 }
 
-const startGame = () => {
-    playerHand = [drawRandomCard(deck), drawRandomCard(deck)]
-    dealerHand = [drawRandomCard(deck), drawRandomCard(deck)]
-}
-
-
-const getHandValue = (hand) => {
-    let sum = 0
-    for (let i = 0; i <hand.length; i++){
-        sum = sum + hand[i]
-    }
-    return sum
-}
-
-const hit = () => {
-    playerHand.push(drawRandomCard(deck))
-    if (getHandValue(playerHand) > 21) {
-        document.getElementById('game-status').innerHTML = "BUST!"
-    }
-}
-
-const stay = () => {
-    dealerHand.push(drawRandomCard(deck))
-    if (getHandValue(dealerHand) > 21) {
-        console.log("BUST!")
-    }
-}
-
-
-document.getElementById('refresh').addEventListener('click', event => {
-    window.location.reload()
-    }
-)
-
-// document.getElementById('play').addEventListener('click', startGame())
-
-startGame()
-
-document.getElementById('player-hand').innerHTML = playerHand
-document.getElementById('player-hand-value').innerHTML = getHandValue(playerHand)
-document.getElementById('dealer-hand').innerHTML = dealerHand
-document.getElementById('dealer-hand-value').innerHTML = getHandValue(dealerHand)
+// const getHandValue = (hand) => {
+//     let sum = 0
+//     for (let i = 0; i <hand.length; i++){
+//         sum = sum + hand[i]
+//     }
+//     return sum
+// }
