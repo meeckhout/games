897b1f5271120ccfb7e568ca89230b36f24c05dd
meeckhout <marthe.eeckhout@gmail.com>
2022-06-15 10:39:05 +0200

    Add function

--- 3. Memory Game/index.js
+++ 3. Memory Game/index.js
@@ -1,5 +1,5 @@
 const section = document.querySelector('section')
-const playerLivesCount = document.querySelector('span')
+const playerLivesCount = document.querySelector('.playerLivesCount')
 const playerLives = 6
 
 // Link text
@@ -15,10 +15,38 @@ const getData = () => [
     {imgSrc: './images/otter.6.jpeg', name: 'otter 6'},
     {imgSrc: './images/otter.7.jpg', name: 'otter 7'},
     {imgSrc: './images/otter.8.jpg', name: 'otter 8'},
+    {imgSrc: './images/otter.1.jpg', name: 'otter 1'},
+    {imgSrc: './images/otter.2.jpg', name: 'otter 2'},
+    {imgSrc: './images/otter.3.png', name: 'otter 3'},
+    {imgSrc: './images/otter.4.jpg', name: 'otter 4'},
+    {imgSrc: './images/otter.5.jpg', name: 'otter 5'},
+    {imgSrc: './images/otter.6.jpeg', name: 'otter 6'},
+    {imgSrc: './images/otter.7.jpg', name: 'otter 7'},
+    {imgSrc: './images/otter.8.jpg', name: 'otter 8'},
 ]
 
 //Randomise
 const randomise = () => {
   const cardData = getData()
     cardData.sort(() => Math.random() - 0.5)
+  return cardData
 }
+
+//Card generator function
+const cardGenerator = () => {
+  const cardData = randomise();
+  //Generate HTML
+  cardData.forEach(item => {
+    console.log(item)
+  })
+
+  const card = document.createElement('div')
+  const face = document.createElement('img')
+  const back = document.createElement('div')
+  card.classList = 'card'
+  face.classList = 'face'
+  back.classList = 'back'
+
+}
+
+cardGenerator()
